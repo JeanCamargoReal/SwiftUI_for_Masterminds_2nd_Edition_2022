508904479bbca6db7ef094fd2613735a9280cdde
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-08-07 10:37:35 -0300

    Defining a multple states

--- DeclarativeUserInterface/DeclarativeUserInterface/ContentView.swift
+++ DeclarativeUserInterface/DeclarativeUserInterface/ContentView.swift
@@ -10,13 +10,16 @@ import SwiftUI
 struct ContentView: View {
 
 	@State private var title: String = "Default Title"
+	@State private var titleActive: Bool = false
 
 	var body: some View {
 		VStack {
 			Text(title)
 				.padding(10)
+				.foregroundColor(titleActive ? .red : .gray)
 			Button {
 				title = "My New Title"
+				titleActive = true
 			} label: {
 				Text("Change Title")
 			}

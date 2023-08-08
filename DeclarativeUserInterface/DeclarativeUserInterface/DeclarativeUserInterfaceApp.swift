6fadc74e01ec1bbbd1b00815a3f32e8d06312769
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-08-08 14:52:15 -0300

    add MVVMExemple project

--- DeclarativeUserInterface/DeclarativeUserInterface/DeclarativeUserInterfaceApp.swift
+++ DeclarativeUserInterface/DeclarativeUserInterface/DeclarativeUserInterfaceApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct DeclarativeUserInterfaceApp: App {
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+			ContentView()
         }
     }
 }
---
+++ MVVMExemple/MVVMExemple/MVVMExempleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMExempleApp.swift
+//  MVVMExemple
+//
+//  Created by Jean Camargo on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMExempleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

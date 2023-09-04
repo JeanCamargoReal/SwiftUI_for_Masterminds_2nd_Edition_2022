97b4b57aed76876109f67265c227db44fede1420
Jean Camargo <jeancamargoreal@icloud.com>
2023-09-04 08:45:43 -0300

    Storing our data in an observable object

---
+++ MVVMExemple/MVVMExemple/ApplicationData.swift
@@ -0,0 +1,13 @@
+//
+//  ApplicationData.swift
+//  MVVMExemple
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 04/09/23.
+//
+
+import SwiftUI
+
+class ApplicationData: ObservableObject {
+    @Published var title: String = "Default Title"
+    @Published var titleInput: String = ""
+}

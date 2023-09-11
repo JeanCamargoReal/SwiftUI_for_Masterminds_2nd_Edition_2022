6cd03685983b25e2697a416c1ce971689b7c2acc
Jean Camargo <jeancamargoreal@icloud.com>
2023-09-11 14:08:30 -0300

    Disabling a button

--- ControlViews/ControlViews/ContentView.swift
+++ ControlViews/ControlViews/ContentView.swift
@@ -8,23 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var colorActive: Bool = false
+    @State private var color = Color.clear
+    @State private var buttonDisabled = false
 
     var body: some View {
         VStack(spacing: 10) {
             Text("Default Title")
                 .padding()
-                .background(colorActive ? Color.green : Color.clear)
-            Button("Change Color", action: changeColor)
+                .background(color)
+            Button("Change Color") {
+                color = Color.green
+                buttonDisabled = true
+            }
+            .disabled(buttonDisabled)
             Spacer()
         }
         .padding()
-    }
-}
-
-extension ContentView {
-    func changeColor() {
-        colorActive.toggle()
     }
 }
 

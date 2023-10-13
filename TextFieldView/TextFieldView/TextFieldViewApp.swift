9d38eebbc41b40021af4615e37b3619ad084f2c8
Jean Camargo <jeancamargoreal@icloud.com>
2023-10-13 11:51:24 -0300

    Create TextFieldView project

---
+++ TextFieldView/TextFieldView/TextFieldViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldViewApp.swift
+//  TextFieldView
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

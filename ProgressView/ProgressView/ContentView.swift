87d098e72f8c5dc5823215f720546842b80f5499
Jean Camargo <jeancamargoreal@icloud.com>
2023-10-19 16:31:09 -0300

    ProgressView - Showing progress

---
+++ ProgressView/ProgressView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProgressView
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 19/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var currentValue: Float = 5
+
+    var body: some View {
+        VStack {
+            ProgressView(value: currentValue, total: 10)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProgressView/ProgressView/ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewApp.swift
+//  ProgressView
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

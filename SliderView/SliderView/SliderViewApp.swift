57b3891688c9dd1ad243726c1e549a82bb71c12f
Jean Camargo <jeancamargoreal@icloud.com>
2023-10-19 14:04:23 -0300

    Create SliderView project

---
+++ SliderView/SliderView/SliderViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderViewApp.swift
+//  SliderView
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SliderViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

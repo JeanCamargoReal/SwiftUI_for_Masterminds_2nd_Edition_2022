bb89e1250b63719d786a807314934641530c2fd8
Jean Camargo <jeancamargoreal@icloud.com>
2023-10-19 14:17:35 -0300

    Creating a slider

--- SliderView/SliderView/ContentView.swift
+++ SliderView/SliderView/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentValue: Float = 5
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Current Value: \(currentValue.formatted(.number.precision(.fractionLength(0))))")
+            Slider(value: $currentValue, in: 0...10, step: 1.0)
+            Spacer()
         }
         .padding()
     }

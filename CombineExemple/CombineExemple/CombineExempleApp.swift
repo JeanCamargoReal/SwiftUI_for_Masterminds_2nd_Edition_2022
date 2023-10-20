5af0e06a66822b424ce13f816f40509fb2230019
Jean Camargo <jeancamargoreal@icloud.com>
2023-10-20 13:53:08 -0300

    Create CombineExemple project

---
+++ CombineExemple/CombineExemple/CombineExempleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineExempleApp.swift
+//  CombineExemple
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineExempleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

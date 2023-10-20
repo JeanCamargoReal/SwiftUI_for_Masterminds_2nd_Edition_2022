bc95d43d7dee44a5fdc24175c517d8ec458ceb9f
Jean Camargo <jeancamargoreal@icloud.com>
2023-10-20 11:00:40 -0300

    Create GroupBoxView project

---
+++ GroupBoxView/GroupBoxView/GroupBoxViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupBoxViewApp.swift
+//  GroupBoxView
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GroupBoxViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

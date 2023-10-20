5a8d717334e068179b652e4f9ad285df65393078
Jean Camargo <jeancamargoreal@icloud.com>
2023-10-20 11:59:56 -0300

    Defining a group of controls

---
+++ ControlGroupView/ControlGroupView/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  ControlGroupView
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 20/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = "No option Selected"
+
+    var body: some View {
+        VStack {
+            Text(message)
+            ControlGroup {
+                Button {
+                    message = "First Option"
+                } label: {
+                    Label("First", systemImage: "trash")
+                }
+                Button {
+                    message = "Second Option"
+                } label: {
+                    Label("Second", systemImage: "mail")
+                }
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ControlGroupView/ControlGroupView/ControlGroupViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlGroupViewApp.swift
+//  ControlGroupView
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ControlGroupViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

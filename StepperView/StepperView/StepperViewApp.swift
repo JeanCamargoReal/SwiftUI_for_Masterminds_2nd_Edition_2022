44b3602aefae8f2da0da7b95f20f4689e2eec3ae
Jean Camargo <jeancamargoreal@icloud.com>
2023-10-20 09:40:32 -0300

    Create StepperView project

---
+++ StepperView/StepperView/StepperViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperViewApp.swift
+//  StepperView
+//
+//  Created by Jean Paulo Marcel Henrique de Camargo on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct StepperViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

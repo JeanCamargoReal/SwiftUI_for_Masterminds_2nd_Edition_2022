4276cfbda8e9358fcba75fddc3996d62fe7b52b7
Jean Camargo <jeancamargoreal@icloud.com>
2023-10-24 10:24:12 -0300

    Defining publishers and subscribers with operators

--- CombineExemple.playground/Contents.swift
+++ CombineExemple.playground/Contents.swift
@@ -2,16 +2,9 @@ import Foundation
 import Combine
 
 let myPublisher = Just("55")
-
-let processPublisher = Publishers.Map<Just<String>, Int>(upstream: myPublisher, transform: { value in 
-    return Int(value) ?? 0 })
-
-let mySubscriber = Subscribers.Sink<Int, Never>(receiveCompletion: { completion in
-    if completion == .finished {
-        print("Complete")
+    .map { value in
+        return Int(value) ?? 0
     }
-}, receiveValue: { value in
-    print("Value: \(value * 100)")
-})
-
-processPublisher.subscribe(mySubscriber)
+    .sink { value in
+        print("Value \(value * 100)")
+    }

fbcdfc78250177a678e8fa34353b621c34969433
Jean Camargo <jeancamargoreal@icloud.com>
2023-10-24 10:13:35 -0300

    Transforming a value with a publisher

--- CombineExemple.playground/Contents.swift
+++ CombineExemple.playground/Contents.swift
@@ -6,12 +6,12 @@ let myPublisher = Just("55")
 let processPublisher = Publishers.Map<Just<String>, Int>(upstream: myPublisher, transform: { value in 
     return Int(value) ?? 0 })
 
-let mySubscriber = Subscribers.Sink<String, Never>(receiveCompletion: { completion in
+let mySubscriber = Subscribers.Sink<Int, Never>(receiveCompletion: { completion in
     if completion == .finished {
         print("Complete")
     }
 }, receiveValue: { value in
-    print("Value: \(value)")
+    print("Value: \(value * 100)")
 })
 
-myPublisher.subscribe(mySubscriber)
+processPublisher.subscribe(mySubscriber)
